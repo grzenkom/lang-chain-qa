610aeb7e49936e45d36413ded444cd59d1b4f29c
grzenkom <marek.grzenkowicz@roche.com>
2023-08-08 15:08:24 +0200

    Run model locally

--- src/lang_chain_qa/hello.py
+++ src/lang_chain_qa/hello.py
@@ -1,17 +1,27 @@
-# https://stackoverflow.com/questions/76137512/langchain-huggingface-cant-evaluate-model-with-two-different-inputs
-# https://github.com/hwchase17/langchain/blob/0e763677e4c334af80f2b542cb269f3786d8403f/docs/modules/models/llms/integrations/huggingface_hub.ipynb
+# https://www.markhneedham.com/blog/2023/06/23/hugging-face-run-llm-model-locally-laptop/
 
-from langchain import HuggingFaceHub, LLMChain
-from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 
-template = "Question: {question} Answer with as few words as possible."
-prompt_template = PromptTemplate(template=template, input_variables=["question"])
+from langchain.llms import HuggingFacePipeline
+from langchain import PromptTemplate, LLMChain
 
-# this uses HF servers for inference
-# TODO: replace with local model
-llm_model = HuggingFaceHub(repo_id="google/flan-t5-base", model_kwargs={"temperature":0, "max_length":64})
+model_id = "lmsys/fastchat-t5-3b-v1.0"
+model = HuggingFacePipeline.from_model_id(
+    model_id=model_id,
+    task="text2text-generation",
+    model_kwargs={"temperature": 0, "max_length": 1000},
+)
 
-llm_chain = LLMChain(prompt=prompt_template, llm=llm_model)
+text_template = """
+You are a friendly chatbot assistant that responds conversationally to users' questions.
+Keep the answers short, unless specifically asked by the user to elaborate on something.
 
-question = "What color goose berry is?"
-print(llm_chain.run(question))
+Question: {question}
+
+Answer:"""
+
+prompt_template = PromptTemplate(template=text_template, input_variables=["question"])
+
+llm_chain = LLMChain(prompt=prompt_template, llm=model)
+
+question = "What color an apple can be?"
+print(llm_chain(question))
